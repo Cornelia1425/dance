f7f5302d718746cbfe38461a7758ddd43bd6b8d8
yiranshu <shuyiran1425@hotmail.com>
2025-07-13 03:54:46 -0400

    buffer

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      include: ['process', 'buffer', 'util']
-    })
-  ],
-  resolve: {
-    alias: {
-      buffer: 'buffer',
-    },
-  },
+  plugins: [react()],
   define: {
     global: 'globalThis',
     'process.env': {},
   },
   optimizeDeps: {
     include: ['buffer', 'process'],
-  },
-  build: {
-    rollupOptions: {
-      external: ['buffer'],
-    },
   },
 })
